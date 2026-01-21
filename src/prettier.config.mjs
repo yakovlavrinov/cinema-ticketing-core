c39c1c6c77fb6287e046cb9a3cbff23c47c7dc17
Yakov Lavrinov <lawrinov.yackov@yandex.ru>
2026-01-21 11:19:28 +0300

    first commit

---
+++ src/prettier.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import("prettier").Config} */
+
+export default {
+    trailingComma: "none",
+    tabWidth: 4,
+    useTabs: true,
+    semi: false,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    arrowParens: "avoid",
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
+    importOrderCaseInsensitive: true,
+    importOrderParserPlugins: [
+        "classProperties",
+        "decorators-legacy",
+        "typescript"
+    ],
+    importOrder: ["<THIRD_PARTY_MODULES>", "^@/(.*)$", "^../(.*)", "^./(.*)"],
+    plugins: ["@trivago/prettier-plugin-sort-import"]
+}
